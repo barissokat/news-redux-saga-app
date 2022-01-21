a9e77ed354b998fdc77e2fd220bf9b35e98e8ab8
Barış Sokat <barissokat@outlook.com>
2022-01-21 16:17:05 +0300

    Add comments

--- src/index.js
+++ src/index.js
@@ -11,13 +11,16 @@ import App from './App'
 
 import './index.css'
 
+// Redux-Saga handle async function with middlewares
 const sagaMiddleware = createSagaMiddleware()
 
+// Saga middleware should be applied when creating store
 const store = createStore(
   reducer,
   applyMiddleware(sagaMiddleware, logger)
 )
 
+// Root Saga run in saga middleware
 sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
